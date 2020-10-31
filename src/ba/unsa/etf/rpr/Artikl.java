a39acf0db393f4627a3e8cc5ee9c058530771774
Nejra Hosic <nhosic2@etf.unsa.ba>
2020-11-01 00:45:03 +0100

    Napravljene metode za klasu Artikl.

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private final String naziv;
+    private final int cijena;
+    private final String kod;
+
+    public Artikl(String ime,int cij,String k) {
+        naziv=ime;
+        cijena = cij;
+        kod=k;
+    }
+    public String getNaziv() {
+        return naziv;
+    }
+    public int getCijena() {
+        return cijena;
+    }
+    public String getKod() {
+        return kod;
+    }
 }
